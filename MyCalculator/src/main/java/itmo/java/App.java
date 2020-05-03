9b8b880b33bcc078fb3fade0954d6513a3a1506b
ayfokin <ayfokin@mail.ru>
2020-05-04 01:04:08 +0300

    Added power feature

--- MyCalculator/src/main/java/itmo/java/App.java
+++ MyCalculator/src/main/java/itmo/java/App.java
@@ -60,6 +60,11 @@ class Calculator {
             case '/':
                 result = Actions.division(number1, number2);
                 break;
+            case '$':
+                result = (int) Actions.root(number1, number2);
+                break;
+            case '^':
+                result = (int) Actions.power(number1, number2);
             default:
                 result = 0;
                 break;
@@ -120,6 +125,10 @@ class Actions {
      */
     static double root(final int a, final int b) {
         return Math.pow(a, 1 / b);
+    }
+
+    static double power(final int a, final int b) {
+        return Math.pow(a, b);
     }
 }
 
